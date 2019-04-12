c86a898a8021fddb59ce9d32db5fedca543cb43e
Dan Anderson <dan@kn0x.io>
2019-04-12 16:39:28 -0400

    add PartialOrd, Ord to OID

--- src/lib.rs
+++ src/lib.rs
@@ -216,7 +216,7 @@ impl PartialEq for ASN1Block {
 }
 
 /// An ASN.1 OID.
-#[derive(Clone,Debug,PartialEq,Eq)]
+#[derive(Clone,Debug,PartialEq,Eq,PartialOrd,Ord)]
 pub struct OID(pub Vec<BigUint>);
 
 impl OID {
